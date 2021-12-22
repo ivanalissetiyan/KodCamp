6751dd6f6a3667b40e9b484c23466fcb2374c425
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2021-12-22 13:30:09 +0700

    install and integrate sosialite

--- resources/views/auth/user/login.blade.php
+++ resources/views/auth/user/login.blade.php
@@ -7,8 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"
+        integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
     <link rel="stylesheet" href="{{ asset('css/main.css') }}" type="text/css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -33,7 +34,7 @@
                 Belajar Coding Dengan Standart Kompetensi Kerja
             </p>
             <p>
-                <a class="btn btn-border btn-google-login" href="#">
+                <a class="btn btn-border btn-google-login" href="{{ route('user.login.google') }}">
                     <img src="{{ asset('images/ic_google.svg') }}" class="icon" alt=""> Sign In with Google
                 </a>
             </p>
@@ -44,8 +45,8 @@
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous">
     </script>
 
 </body>
