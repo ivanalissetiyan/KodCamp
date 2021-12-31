f340f45912ce8a35bbf8071eca8773d09c0bc689
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2021-12-31 14:26:46 +0700

    validation checkout

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\Checkout;
 use Illuminate\Http\Request;
+use App\Http\Requests\User\Checkout\Store;
 use App\Models\Camp;
 use Illuminate\Support\Facades\Auth;
 
@@ -45,9 +46,9 @@ class CheckoutController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, Camp $camp)
+    public function store(Store $request, Camp $camp)
     {
-
+        return ($request);
         // mapping request data
         $data = $request->all();
         $data['user_id'] = Auth::id();
