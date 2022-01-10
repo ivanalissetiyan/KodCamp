cff4f2adb8547f59baa45772b2591d7157c7e555
ivanalissetiyan <ivanalissetiyan1998@gmail.com>
2022-01-10 14:37:16 +0700

    init midtrans

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,8 @@ class User extends Authenticatable
         'email',
         'password',
         'pekerjaan',
+        'phone',
+        'address',
         'avatar',
         'is_admin',
         'email_verified_at',
